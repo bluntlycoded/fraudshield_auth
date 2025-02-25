4db7a2d61f1aa3142226833baecc23a075e0f3a1
Rahul Nayak <114763168+bluntlycoded@users.noreply.github.com>
2025-02-25 13:11:38 +0530

    Update run.py

--- run.py
+++ run.py
@@ -11,4 +11,4 @@ def index():
 socketio = SocketIO(app)
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=8000, debug=False)  # Change host to 0.0.0.0
+    socketio.run(app, host="0.0.0.0", port=8000, debug=False,allow_unsafe_werkzeug=True)  # Change host to 0.0.0.0
