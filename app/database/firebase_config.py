7fa6405faa333b589c4d41156f326ee4e5dddf5c
Rahul Nayak <114763168+bluntlycoded@users.noreply.github.com>
2025-03-04 08:26:34 +0530

    Update firebase_config.py

--- app/database/firebase_config.py
+++ app/database/firebase_config.py
@@ -3,6 +3,6 @@ from firebase_admin import credentials, firestore, auth
 import os
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(firebase_credentials.json)
+    cred = credentials.Certificate("./firebase_credentials.json")
     firebase_admin.initialize_app(cred)
 db = firestore.client()
