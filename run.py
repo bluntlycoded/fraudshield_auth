0f299df4766376ca33db2ec33d7b3f17b5370eb4
Rahul Nayak <114763168+bluntlycoded@users.noreply.github.com>
2025-03-04 17:30:59 +0530

    Update run.py

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from flask_socketio import SocketIO
 app = create_app()
 socketio = SocketIO(app)
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=8000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=8000,allow_unsafe_werkzeug=True)
